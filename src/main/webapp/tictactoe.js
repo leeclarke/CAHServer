b9cbebb13c5ba709f95a56bbcfc900bcd1f41e94
lees2bytes <lees2bytes.gmail.com>
2013-08-20 21:28:55 -0400

    add card shuffle2

--- src/main/webapp/tictactoe.js
+++ src/main/webapp/tictactoe.js
@@ -96,14 +96,14 @@ console.log('Creating TicTacToe object');
  * Using Fisher-Yates shuffle algorithm.
  */
   shuffleCards: function(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-    return array;
-}
+      for (var i = array.length - 1; i > 0; i--) {
+          var j = Math.floor(Math.random() * (i + 1));
+          var temp = array[i];
+          array[i] = array[j];
+          array[j] = temp;
+      }
+      return array;
+  },
 
     /**
      * Channel opened event; checks number of open channels.
