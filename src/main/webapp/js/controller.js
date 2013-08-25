19a1ce68263d7887cc986a7f33421b7676c10dbb
lees2bytes <lees2bytes.gmail.com>
2013-08-24 23:16:57 -0400

    bootstrtapped

--- src/main/webapp/js/controller.js
+++ src/main/webapp/js/controller.js
@@ -13,10 +13,7 @@ function GameDisplayCtrl($scope) {
 
       //mBoard.clear();
       //mBoard.drawGrid();
-      var gameEngine = new cast.TicTacToe();
-      gameEngine.mChannelHandler.addChannelFactory(
-          chromecastApp.createChannelFactory(cast.TicTacToe.PROTOCOL));
+      $scope.game = new cast.TicTacToe();
+      $scope.game.mChannelHandler.addChannelFactory(chromecastApp.createChannelFactory(cast.TicTacToe.PROTOCOL));
       chromecastApp.start();
-
-      $scope.game = gameEngine;
 }
